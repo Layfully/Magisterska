7534f00c4900820a5c5134aa68aa7da42211a3e7
Adrian Gaborek <A.Gaborek@devnet.de>
2025-02-09 00:05:18 +0100

    add missing benchmarks

--- master-thesis/Benchmark.cs
+++ master-thesis/Benchmark.cs
@@ -1,14 +1,70 @@
 ﻿using BenchmarkDotNet.Attributes;
+using GeneticSharp;
+using System;
 
 namespace master_thesis;
 
 public class Benchmark
 {
-    [Benchmark]
-    public void Test()
+    private GeneticAlgorithm ga;
+    private string qwertyLayout = "uakft,gsxpnw/dq.iymljye;crzhob";
+    private KeyboardFitness fitness;
+    private KeyboardChromosome chromosome;
+
+    [GlobalSetup]
+    public void Setup()
     {
-        string qwerty = "qazwsxedcrfvtgbyhnujmik,ol.p;/";
+        fitness = new KeyboardFitness("E:\\CSharpDataset");
+        chromosome = new KeyboardChromosome(qwertyLayout);
+    }
 
+    [Benchmark]
+    public void RunGeneticAlgorithm_Population10_Mutation0_1()
+    {
+        SetupGeneticAlgorithm(10, 0.1f);
+        ga.Start();
+    }
 
+    [Benchmark]
+    public void RunGeneticAlgorithm_Population20_Mutation0_2()
+    {
+        SetupGeneticAlgorithm(20, 0.2f);
+        ga.Start();
+    }
+
+    [Benchmark]
+    public void RunGeneticAlgorithm_Population50_Mutation0_3()
+    {
+        SetupGeneticAlgorithm(50, 0.3f);
+        ga.Start();
+    }
+
+    [Benchmark]
+    public void RunGeneticAlgorithm_Population100_Mutation0_4()
+    {
+        SetupGeneticAlgorithm(100, 0.4f);
+        ga.Start();
+    }
+
+    [Benchmark]
+    public void RunGeneticAlgorithm_Population200_Mutation0_5()
+    {
+        SetupGeneticAlgorithm(200, 0.5f);
+        ga.Start();
+    }
+
+    private void SetupGeneticAlgorithm(int populationSize, float mutationProbability)
+    {
+        EliteSelection selection = new();
+        KeyboardCrossover crossover = new();
+        KeyboardMutation mutation = new();
+
+        Population population = new(populationSize, populationSize, chromosome);
+
+        ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation)
+        {
+            Termination = new FitnessStagnationTermination(100),
+            MutationProbability = mutationProbability
+        };
     }
 }
